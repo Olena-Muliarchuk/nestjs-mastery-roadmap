b6a1ed59b29fde76c26a9c223a7cb4e48fab7ebb
Olena Muliarchuk <olena.muliarchuk.r@gmail.com>
2026-01-27 16:57:47 +0200

    add CRUD operation for songs

--- src/songs/songs.controller.ts
+++ src/songs/songs.controller.ts
@@ -1,9 +1,21 @@
-import { Controller, Get, Post, Body, Query, DefaultValuePipe, ParseIntPipe } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Query,
+  DefaultValuePipe,
+  ParseIntPipe,
+  Param,
+  Put,
+  Delete,
+} from '@nestjs/common';
 import { SongsService } from './songs.service';
 import { CreateSongDto } from './dto/create-song.dto';
 import { Song } from './song.entity';
 import { Pagination } from 'nestjs-typeorm-paginate';
 import { ConfigService } from '@nestjs/config';
+import { UpdateSongDto } from './dto/update-song.dto';
 
 @Controller('songs')
 export class SongsController {
@@ -31,5 +43,20 @@ export class SongsController {
   @Post()
   create(@Body() createSongDto: CreateSongDto): Promise<Song> {
     return this.songsService.create(createSongDto);
+  }
+
+  @Get(':id')
+  findOne(@Param('id', ParseIntPipe) id: number): Promise<Song> {
+    return this.songsService.findOne(id);
+  }
+
+  @Put(':id')
+  update(@Param('id', ParseIntPipe) id: number, @Body() updateSongDto: UpdateSongDto) {
+    return this.songsService.update(id, updateSongDto);
+  }
+
+  @Delete(':id')
+  delete(@Param('id', ParseIntPipe) id: number) {
+    return this.songsService.delete(id);
   }
 }
--- src/songs/songs.service.ts
+++ src/songs/songs.service.ts
@@ -1,9 +1,10 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { CreateSongDto } from './dto/create-song.dto';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { DeleteResult, Repository, UpdateResult } from 'typeorm';
 import { Song } from './song.entity';
 import { paginate, Pagination, IPaginationOptions } from 'nestjs-typeorm-paginate';
+import { UpdateSongDto } from './dto/update-song.dto';
 
 @Injectable()
 export class SongsService {
@@ -20,5 +21,36 @@ export class SongsService {
 
   async paginate(options: IPaginationOptions): Promise<Pagination<Song>> {
     return paginate<Song>(this.songRepository, options);
+  }
+
+  async findOne(id: number): Promise<Song> {
+    const song = await this.songRepository.findOneBy({ id });
+    if (!song) {
+      throw new HttpException('Song not found', HttpStatus.NOT_FOUND);
+    }
+
+    return song;
+  }
+
+  async update(id: number, recordToUpdate: UpdateSongDto): Promise<UpdateResult> {
+    if (Object.keys(recordToUpdate).length === 0) {
+      throw new HttpException('Provide at least one field to update', HttpStatus.BAD_REQUEST);
+    }
+    const result = await this.songRepository.update(id, recordToUpdate);
+
+    if (result.affected === 0) {
+      throw new HttpException('Song not found', HttpStatus.NOT_FOUND);
+    }
+
+    return result;
+  }
+
+  async delete(id: number): Promise<DeleteResult> {
+    const result = await this.songRepository.delete(id);
+    if (result.affected === 0) {
+      throw new HttpException('Song not found', HttpStatus.NOT_FOUND);
+    }
+
+    return result;
   }
 }
