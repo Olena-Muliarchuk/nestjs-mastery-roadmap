19fa7830bdf9f0fb8f55fba9a1911f5db961cc2b
Olena Muliarchuk <olena.muliarchuk.r@gmail.com>
2026-04-10 21:16:53 +0300

    test: fix broken autogenerated specs by providing dependency mocks

--- src/events/events.gateway.spec.ts
+++ src/events/events.gateway.spec.ts
@@ -1,12 +1,18 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { EventsGateway } from './events.gateway';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 
 describe('EventsGateway', () => {
   let gateway: EventsGateway;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [EventsGateway],
+      providers: [
+        EventsGateway,
+        { provide: JwtService, useValue: {} },
+        { provide: ConfigService, useValue: {} },
+      ],
     }).compile();
 
     gateway = module.get<EventsGateway>(EventsGateway);
